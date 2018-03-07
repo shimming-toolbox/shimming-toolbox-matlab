7c766e873150d226191414ebe125986e4b93c28f
Alexandru Foias <alexandrufoias@gmail.com>
2018-03-07 15:04:10 -0500

    added calibration using arduino serial monitor.

--- arduino/comm_protocol_v2/control2/control2.ino
+++ arduino/comm_protocol_v2/control2/control2.ino
@@ -67,8 +67,8 @@ void loop() {
 
   String inString = "";
   char incomingByte;
-  float p1[] = {0.65909, 0.65, 0.64547, 0.6499, 0.6591, 0.654, 0.65, 0.65};            // Coefficients from Feedback calibration
-  float p2[] = {19.09, -10.908, -23.636, 20.91, 32.728, 11.308, -42.728, 34.546};
+  float p1[] = {0.65, 0.6591, 0.65, 0.65, 0.66818, 0.65, 0.65, 0.65};
+  float p2[] = {24.548, -9.998, 2.728, 25.456, 34.544, 7.272, -19.998, 34.546};
 
   float val0;
   float val;
@@ -88,7 +88,7 @@ void loop() {
       Serial.println("\n");
       resetFunc();
       break;
-      
+
     case 's':              // Update one channel input current
       element = (Serial.parseInt() - 1);
       //Serial.println(element);
@@ -120,7 +120,7 @@ void loop() {
         delay(50);
       }
       break;
-
+    
     case 'x':                 // Calibration of DAC current
 
       for (sch = 1; sch <= 8; sch++)
@@ -134,19 +134,45 @@ void loop() {
           {
             int adc0 = adc1.readADC_SingleEnded(sch - 1);
             Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2);
-            //Serial.println('\n');
           }
           else
           {
             int adc0 = adc2.readADC_SingleEnded(sch - 5);
             Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2);
-            //Serial.println('\n');
           }
         }
 
       }
+      break;
 
-      //Serial.print('\n');
+    case 'c':                 // Calibration of DAC current
+
+      for (sch = 1; sch <= 8; sch++)
+      {
+        Serial.print("ch"); Serial.print(sch); Serial.print("y = [");
+
+        for (n_cal = 0; n_cal < 5; n_cal++)
+        {
+          vOut = ((1.25 - cal_val[n_cal] * 0.001 * 0.22) * 26214);
+          DAC.writeUpdateCh(sch - 1, vOut);
+          delay(1000);
+          if (sch < 5)
+          {
+            int adc0 = adc1.readADC_SingleEnded(sch - 1);
+            Serial.print(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2);
+          }
+          else
+          {
+            int adc0 = adc2.readADC_SingleEnded(sch - 5);
+            Serial.print(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2);
+          }
+          if (n_cal < 4) {
+            Serial.print(", ");
+          }
+        }
+        Serial.print("];");
+        Serial.println('\n');
+      }
       break;
 
     case 'o':                // Update all channels with 8 currents
