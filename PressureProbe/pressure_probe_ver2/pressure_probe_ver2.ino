5f5cc41d608c1583971e0a0b134a860bf4af5d20
alexfoias <alexfoias@users.noreply.github.com>
2018-07-18 11:57:47 -0400

    updated baudrate

--- PressureProbe/pressure_probe_ver2/pressure_probe_ver2.ino
+++ PressureProbe/pressure_probe_ver2/pressure_probe_ver2.ino
@@ -1,14 +1,14 @@
 int pressurePin = A5; // pressure sensor
 
 void setup() {
-  Serial.begin(2000000);
+  Serial.begin(115200);
   analogReference(EXTERNAL);
 
 }
 
 void loop() {
       Serial.println(analogRead(pressurePin));
-      delay(1);
+      delay(10);
       }
 
 
