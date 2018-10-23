dffbfa4178ccbf76590184396dbf4df8c5bc9397
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2018-10-23 00:06:52 -0400

    Remove superfluous elements to ACDC shim communication + Arduino microcontroller source code

--- ShimControl/ShimControl.ino
+++ ShimControl/ShimControl.ino
@@ -2,7 +2,7 @@
   ShimControl.ino
 
 // =========================================================================
-// Updated::20180727::ryan.topfer@polymtl.ca
+// Updated::20181022::ryan.topfer@polymtl.ca
 // =========================================================================
 */
 
@@ -33,11 +33,18 @@ Adafruit_ADS1015 adc2 (0x49); /* Use thi for the 12-bit version */
 AD5668 DAC = AD5668(mosiPin, sclkPin , ssPin, clrPin, ldacPin);
 
 // 3 terms describing hardware:
+const uint8_t ADC_NCHANNELS       = 4 ; 
+const uint8_t ADC_RESOLUTION      = 12 ; // 12-bit
+const float   ADC_RANGE_VOUT      = 2.048 ; // for GAIN_TWO setting [units: volts]
+// 1 derived term for convenience:
+const float ADC_VOLTSPERBIT       = ADC_RANGE_VOUT/( pow( 2.0, float(ADC_RESOLUTION-1) ) - 1.0 ) ;
+
+// 3 terms describing hardware:
 const uint8_t DAC_RESOLUTION      = 16 ; // 16-bit
 const float DAC_VREF              = 1.25 ; // [units: volts]
 const float DAC_PREAMP_RESISTANCE = 0.22 ; // [units: Ohms]
 
-// 3 derived terms (for convenience):
+// 3 derived terms for convenience:
 const float DAC_RANGE_VOUT  = 2.0*DAC_VREF ; // [units: volts]
 const float DAC_BITSPERVOLT = ( pow( 2.0, float(DAC_RESOLUTION) ) - 1.0 )/DAC_RANGE_VOUT ; // =26214.0 [units: bit-counts]
 const float DAC_ZERO        = DAC_VREF*DAC_BITSPERVOLT ; // =32767.5 [units: bit-counts]
@@ -57,102 +64,62 @@ void setup() {
 
   DAC.powerDAC_Normal(B11111111); // Power up all channels normal
 
-  Serial.println("Setting ADC Range: +/- 2.048V  (1 bit = 1mV)");
+  Serial.println("Setting ADC Range: +/- 2.048V  (1 bit = 1.0mV)");
   adc1.setGain(GAIN_TWO); //+/- 2.048V  1 bit = 1mV
   adc2.setGain(GAIN_TWO); //+/- 2.048V  1 bit = 1mV
 
   adc1.begin();
   adc2.begin();
-  
-    Serial.println("Ready to receive commands ...");
+
+  resetallshims() ;
+    
+  Serial.println("Ready to receive commands ...");
 }
 
 void(* resetFunc) (void) = 0;//declare reset function at address 0
 
 void loop() {
-  //Declaration of global variables 
 
-  int sch;
-  uint8_t iCh ;
   int element;
   int data;
   int i;
   int bytesread;
-  int n_cal;
   unsigned int DACvaluetosend [8];                                                   //Array to store the 8 currents converted in DAC values
-  int16_t adc0;
   int64_t a [41];                                                                    // Array to store 40 digits from serial communication
 
   String inString = "";
   char incomingByte;
 
   float val;
-  float req_val;
-  float cal_val [] = { -200, -100, 0, 100, 200};                                    // Value for Feedback calibration
+  
   float p1[] = {0.65909, 0.65, 0.64547, 0.6499, 0.6591, 0.654, 0.65, 0.65};            // Coefficients from Feedback calibration
   float p2[] = {19.09, -10.908, -23.636, 20.91, 32.728, 11.308, -42.728, 34.546};
   float D [41];                                                                        // Array to store 40 digits from serial communication
   float currents [8];                                                                  //Array to store the 8 currents
-  float vOut;
-
-
 
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
   }
-  switch (incomingByte) {
+  switch (incomingByte) 
+  {
 
-    case 'a':              // Update one channel input current 
-      iCh = (Serial.parseInt()-1);
-      //Serial.println(element);
-      val = Serial.parseFloat();
-      //Serial.println(val);
-      req_val = (val - p2[element]) / p1[element];   
-      setandloadchannel( iCh, req_val);
+    /* case 'a':              // Update one channel input current  */
+    /*   uint8_t iCh ; */
+    /*  float req_val ; */
+    /*    */
+    /*   iCh = (uint8_t)(Serial.parseInt()- 1) ; */
+    /*   val = Serial.parseFloat(); */
+    /*   Serial.println(val); */
+    /*   req_val = (val - p2[element]) / p1[element];    */
+    /*   DAC.writeUpdateCh( iCh, ampstodac(req_val) );   */
+    /*   break; */
+
+    case 'x':                 // Calibration of Adc current feedback
+    calibrateadc() ;
       break;
-    /* case 'x':                 // Calibration of Adc current feedback */
-    /*  */
-    /*   for (sch = 1; sch <= 8; sch++) */
-    /*   { */
-    /*     for (n_cal = 0; n_cal < 5; n_cal++) */
-    /*     { */
-    /*       vOut = ((1.25 - cal_val[n_cal] * 0.001 * 0.22) * 26214); */
-    /*       DAC.writeUpdateCh(sch - 1, vOut); */
-    /*       delay(1000); */
-    /*       if (sch < 5) */
-    /*       { */
-    /*         int adc0 = adc1.readADC_SingleEnded(sch - 1); */
-    /*         Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2); */
-    /*         //Serial.println('\n'); */
-    /*       } */
-    /*       else */
-    /*       { */
-    /*         int adc0 = adc2.readADC_SingleEnded(sch - 5); */
-    /*         Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 2); */
-    /*         //Serial.println('\n'); */
-    /*       } */
-    /*     } */
-    /*  */
-    /*   } */
-    /*  */
-//      //Serial.print('\n'); 
-//      break;
 
     case 'w':                // Set all channels to 0V
-
-    for ( uint8_t iCh = 0; iCh < SHIM_NCHANNELS; iCh++ ) 
-    {
-        DAC.writeUpdateCh( iCh, ampstodac( 0.0 ) ) ;
-    }
-      /* DAC.writeUpdateCh(0, 1.259 * 26214.0); */
-      /* DAC.writeUpdateCh(1, 1.246 * 26214.0); */
-      /* DAC.writeUpdateCh(2, 1.251 * 26214.0); */
-      /* DAC.writeUpdateCh(3, 1.257 * 26214.0); */
-      /*  */
-      /* DAC.writeUpdateCh(4, 1.259 * 26214.0); */
-      /* DAC.writeUpdateCh(5, 1.253 * 26214.0); */
-      /* DAC.writeUpdateCh(6, 1.244 * 26214.0); */
-      /* DAC.writeUpdateCh(7, 1.262 * 26214.0); */
+        resetallshims( ) ;
 
       break;
       
@@ -165,13 +132,12 @@ void loop() {
       feedback();
       break;
 
-
-    case 'q':                // Querry on currents feedback (Display current with channels info)
-      query();
+    case 'q':                
+      queryallchannelcurrents();
       break;
 
-    case 'y':                // Querry on volatge feedback
-      queryVoltage();
+    case 'y':                
+      queryallchannelvoltages();
       break;
 
     case 'r':                // Reset the arduino
@@ -184,17 +150,17 @@ void loop() {
 
        for ( data= 0; data <= 40; data++){
         
-       bytesread=Serial.read();       // Read digits receive by serial communication one by one
-       if (isDigit(bytesread)) {
-       inString += (char)bytesread;}  //Save these values in a string
-       a[data]=inString.toInt();
-       D[data]=double(a[data]);       //Conversion from string to double
-       Serial.print(D[data]);
-       
-       
-      // Clear the string for new input:
-      inString = "";
-   }
+           bytesread=Serial.read();       // Read digits receive by serial communication one by one
+           if (isDigit(bytesread)) {
+           inString += (char)bytesread;}  //Save these values in a string
+           a[data]=inString.toInt();
+           D[data]=double(a[data]);       //Conversion from string to double
+           Serial.print(D[data]);
+           
+           
+          // Clear the string for new input:
+          inString = "";
+      }
       Serial.println("Coeff saved");
       // Reconstruction of the DAC values to send
       
@@ -228,52 +194,101 @@ void loop() {
   }
 }
 
-void query() {
-  Serial.println("\n");
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
-  {
-    int adc0 = adc1.readADC_SingleEnded(chAdr);
-    Serial.print("CH "); Serial.print(chAdr + 1); Serial.print(" set to : ");
-    Serial.print(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6); Serial.println(" mA");
-  }
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
-  {
-    int adc0 = adc2.readADC_SingleEnded(chAdr);
-    Serial.print("CH "); Serial.print(chAdr + 5); Serial.print(" set to : ");
-    //Serial.print(adc0, 4); Serial.println(" V");
-    Serial.print(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6); Serial.println(" mA");
-  }
-}
+void calibrateadc( void )
+{
+  // test currents [units: A]
+  float calibrationCurrents [] = { -0.2, -0.1, 0.0, 0.1, 0.2 } ; 
+  uint8_t nCalibrationCurrents = 5 ;
 
-void queryVoltage() {
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
+  for ( uint8_t iCh = 0; iCh < SHIM_NCHANNELS; iCh++)
   {
-    int adc0 = adc1.readADC_SingleEnded(chAdr);
-    Serial.print("CH "); Serial.print(chAdr + 1); Serial.print(" set to : ");
-    Serial.print(adc0, 4); Serial.println(" V");
-    //Serial.print((adc0 * 0.001) , 6); Serial.println(" V");
-  }
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
-  {
-    int adc0 = adc2.readADC_SingleEnded(chAdr);
-    Serial.print("CH "); Serial.print(chAdr + 5); Serial.print(" set to : ");
-    Serial.print(adc0, 4); Serial.println(" V");
-    //Serial.print((adc0 * 0.001), 6); Serial.println(" V");
+    for (uint8_t iCalibrationCurrent = 0; iCalibrationCurrent < nCalibrationCurrents; iCalibrationCurrent++)
+    {
+      DAC.writeUpdateCh( iCh, ampstodac( calibrationCurrents[iCalibrationCurrent] ) ) ;
+      delay(1000) ;
+
+      Serial.println( querychannelcurrent( iCh ), 2);
+    }
   }
 }
 
-void setandloadchannel(uint8_t iCh, float current) {
-  float vOut;
-  vOut = ((1.25 - current * 0.001 * 0.22) * 26214); // Convert current to DAC currentue
-
-  DAC.writeUpdateCh( iCh, vOut);             // Update channel 
+void queryallchannelcurrents() 
+{
+    for (uint8_t iCh = 0; iCh < SHIM_NCHANNELS; iCh++)
+    { 
+        Serial.print("CH ") ; Serial.print(iCh + 1) ; Serial.print(" set to : ") ;
+        Serial.print( querychannelcurrent( iCh ) , 5); Serial.println(" A");
+    } 
 }
 
-unsigned int ampstodac( float current ) {
-
-    return round( DAC_PREAMP_RESISTANCE*current*DAC_BITSPERVOLT - DAC_ZERO ) ;
-
+void queryallchannelvoltages( void ) 
+{
+    for ( uint8_t iCh = 0; iCh < SHIM_NCHANNELS; iCh++ )
+    {
+        Serial.print("CH ") ; Serial.print(iCh + 1) ; Serial.print(" set to : ") ;
+        Serial.print( querychannelvoltage( iCh ), 4); Serial.println(" V");
+    } 
 }
+
+float querychannelcurrent( uint8_t iChannel ) 
+{
+    return (querychannelvoltage( iChannel ) - DAC_VREF)/DAC_PREAMP_RESISTANCE ;
+}
+
+float querychannelvoltage( uint8_t iChannel ) 
+{
+    return ADC_VOLTSPERBIT * float( readchanneladc( iChannel ) ) ; 
+}
+
+
+
+uint16_t readchanneladc( uint8_t iChannel ) 
+{
+    if ( iChannel < ADC_NCHANNELS )                                  
+        return adc1.readADC_SingleEnded( iChannel ) ;
+    else if ( iChannel < 2*ADC_NCHANNELS )
+        return adc2.readADC_SingleEnded( iChannel - ADC_NCHANNELS ) ;
+    else
+		Serial.print("Error: Invalid channel index. ") ;
+}
+
+void resetallshims( void ) 
+{
+    for ( uint8_t iCh = 0; iCh < SHIM_NCHANNELS; iCh++ ) 
+    {
+        DAC.writeUpdateCh( iCh, ampstodac( 0.0 ) ) ;
+    }
+}
+
+
+
+
+
+
+void feedback(){
+  for (int chAdr = 0; chAdr <= 3; chAdr++)
+  {
+    uint16_t adc0 = adc1.readADC_SingleEnded(chAdr);
+    Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6);
+  }
+  for (int chAdr = 0; chAdr <= 3; chAdr++)
+  {
+    uint16_t adc0 = adc2.readADC_SingleEnded(chAdr);
+    Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6);
+  }
+}
+
+unsigned int ampstodac( float current ) 
+{
+    return round( current*DAC_PREAMP_RESISTANCE*DAC_BITSPERVOLT - DAC_ZERO ) ;
+}
+
+/* float adctoamps( uint16_t adcCounts ) { */
+/*  */
+/*     return round( adcCounts */
+/*     return round( DAC_PREAMP_RESISTANCE*current*DAC_BITSPERVOLT - DAC_ZERO ) ; */
+/*  */
+/* } */
 
 //void feedback(int element){
   
@@ -281,15 +296,3 @@ unsigned int ampstodac( float current ) {
 //   Serial.print(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6); //Display current feedback 
 //}
 
-void feedback(){
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
-  {
-    int adc0 = adc1.readADC_SingleEnded(chAdr);
-    Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6);
-  }
-  for (int chAdr = 0; chAdr <= 3; chAdr++)
-  {
-    int adc0 = adc2.readADC_SingleEnded(chAdr);
-    Serial.println(((adc0 * 0.001) - 1.25) / 0.22 * 1000, 6);
-  }
-}
