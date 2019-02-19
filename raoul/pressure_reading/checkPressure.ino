21c3db77e948288087b4c641a04e04fd542bf004
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-02-19 11:42:22 -0500

    raoul/: Removed because not used in regular basis.
    Code available at: https://osf.io/acgjk/

--- raoul/pressure_reading/checkPressure.ino
+++
@@ -1,9 +0,0 @@
-float checkPressure() 
-{
-
-  pinBits    = analogRead(pressurePin);          // read the input pin
-  pinVoltage = float ( pinBits ) * mVPerAdcBit ; // voltage on the ADC pin
-  return pinVoltage * mBarPermV ;                // returns pressure in mBar
-
-    /* return ssc.pressure() ;  */
-}
--- raoul/valve_control/integrated_control_v2/startMessage.ino
+++
@@ -1,9 +0,0 @@
-void startMessage() {
-  //initial message
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Phantom");
-  lcd.setCursor(0, 1);
-  lcd.print("Raoul");
-  delay(3000);
-}
