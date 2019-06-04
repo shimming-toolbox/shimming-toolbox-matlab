9f06636de435989a5a918bd17ca160cbfa1e80f2
Nicolas-Pinon <nicolas.e.pinon@gmail.com>
2019-06-04 16:28:23 -0400

    Update get_capacitive_probe_signal.m
    Fitting now updated every period, also name changes for clarity

--- RespiratoryProbe/get_capacitive_probe_signal.m
+++ RespiratoryProbe/get_capacitive_probe_signal.m
@@ -10,30 +10,37 @@ n_samples_before_start_fitting = 50;
 deg_poly = 0;
 
 timepoint = 1;
+count_zero_cross = 0;
 FS = stoploop({'Stop cprobe'}) ;
 while(~FS.Stop())
     % define signal from time 1 to time timepoint
-    signal_norm = data_cprobe(1:timepoint);
+    signal_raw = data_cprobe(1:timepoint);
 
     % Update polynomial degree for normalization
     deg_poly = update_deg_poly(deg_poly, timepoint);
     
     % Display stuff
-    if timepoint > n_samples_before_start_fitting
-        [signal_norm, signal_fit] = update_fit(signal_norm, 1, deg_poly);
+    if timepoint > n_samples_before_start_fitting   
+        [signal_hf, signal_lf] = separate_lf_hf(signal_raw, 1, stop_time, deg_poly);
+        if zero_cross(signal_hf)
+            count_zero_cross = count_zero_cross + 1;
+            if rem(count_zero_cross, 2) == 0
+                stop_time = timepoint;
+            end
+        end
         % Display
         figure(1)
         x = 1:timepoint;
         subplot(2,1,1)
         plot(x, data_cprobe(x), 'k');
         hold on
-        plot(x, signal_fit, 'r');
+        plot(x, signal_lf, 'r');
         hold off
-        title('Raw signal')
+        title(['Raw signal estimated frequency is : ', num2str(meanfreq(signal_raw))])
         ylabel('Frequency [KHz]')
         grid on
         subplot(2,1,2)
-        plot(x, signal_norm, 'k');
+        plot(x, signal_hf, 'k');
         title(['Normalized signal (deg\_poly=' num2str(deg_poly) ')'])
         xlabel('Time [s]')
         grid on
@@ -44,21 +51,30 @@ while(~FS.Stop())
         subplot(2,1,1)
         plot(x, data_cprobe(x), 'k');
         grid on
+        stop_time = timepoint;
     end
     timepoint = timepoint + 1;
  
 end
 end
 
+function [zero_cross] = zero_cross(signal_in)
+product = signal_in(end) .* signal_in(end-1);
+if product > 0
+    zero_cross = 0;
+else
+    zero_cross = 1;
+end
+end
 
-function [signal_norm, signal_fit] = update_fit(signal_in, x_start, deg_poly)
-% Update the fit and display fig
-x = x_start:length(signal_in);
-p = polyfit(x, signal_in(x_start:end), deg_poly);
+function [signal_hf, signal_lf] = separate_lf_hf(signal_in, x_start, x_stop, deg_poly)
+% Polynomial fitting for separating low and high frequency
+x = x_start:x_stop;
+p = polyfit(x, signal_in(x_start:x_stop), deg_poly);
 % define x variable based on signal_in
 x_full = 1:length(signal_in);
-signal_fit = polyval(p, x_full);
-signal_norm = signal_in - signal_fit;
+signal_lf = polyval(p, x_full);
+signal_hf = signal_in - signal_lf;
 end
 
 
