5c3b29d6bff31dd974e9db37c05030c4d0acc517
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-06-12 17:44:03 -0400

    filter_signal.m: New function to filter probe signal

---
+++ RespiratoryProbe/filter_signal.m
@@ -0,0 +1,5 @@
+function filter_signal(probe_type, signal)
+% Filter the input probe signal for drifts and high frequency glitches.
+% if signal is a string, it is considered as a file name. If it is a vector
+% it is considered as the raw signal to filter.
+
