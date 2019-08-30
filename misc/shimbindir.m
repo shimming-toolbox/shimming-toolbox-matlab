835798c69f889ba1764c659c043c904bdb1a3fe0
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2019-08-30 19:12:24 -0400

    add shimbindir(): function returns directory containing shim reference map binaries

---
+++ misc/shimbindir.m
@@ -0,0 +1,16 @@
+function shimDir = shimbindir( )
+%SHIMBINDIR  Returns binary directory of shim reference maps as a string
+%
+% shimDir = SHIMBINDIR( ) ;
+%
+% by default, shimDir = '~/Matlab/shimReferenceMaps/'
+%
+% If you want to store the binaries in a different directory, replace 
+% the corresponding entry in this function with the appropriate directory.
+
+% change this if the reference map binaries are to be stored elsewhere:
+shimDir = '~/Matlab/shimReferenceMaps/' ;
+
+assert( exist(shimDir) == 7, 'Shim binary directory not found.' ) ;
+
+end
