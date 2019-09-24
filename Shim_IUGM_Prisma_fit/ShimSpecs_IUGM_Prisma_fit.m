84081d12ad43b15594cdd5be3867563eefa25d76
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2019-09-23 20:29:45 -0400

    ShimSpecs_IUGM_Prisma_fit: Clean-up + transfer .converttomultipole() method from ShimOpt_IUGM_Prisma_fit

--- Shim_IUGM_Prisma_fit/ShimSpecs_IUGM_Prisma_fit.m
+++ Shim_IUGM_Prisma_fit/ShimSpecs_IUGM_Prisma_fit.m
@@ -6,7 +6,7 @@ classdef ShimSpecs_IUGM_Prisma_fit < ShimSpecs
 %    ShimSpecs_IUGM_Prisma_fit is a ShimSpecs subclass
 %
 % =========================================================================
-% Updated::20180821::ryan.topfer@polymtl.ca
+% Author::ryan.topfer@polymtl.ca
 % =========================================================================
 
 % =========================================================================
@@ -17,11 +17,12 @@ function Shim = ShimSpecs_IUGM_Prisma_fit(  )
 %SHIMSPECS - Shim System Specifications 
 
 Shim.Id.systemName   = 'IUGM_Prisma_fit' ;
-Shim.Id.channelNames = { 'X' ; 'Y' ; 'Z' ; 'A20' ; 'A21' ; 'B21' ; 'A22' ; 'B22' } ;
+Shim.Id.channelNames = { 'X (A11)' ; 'Y (B11)' ; 'Z (A10)' ; 'Z2 (A20)' ; 'ZX (A21)' ; 'ZY (B21)' ; 'X2-Y2 (A22)' ; 'XY (B22)' } ;
 Shim.Id.channelUnits = { '[micro-T/m]' ; '[micro-T/m]'; '[micro-T/m]'; 
     '[micro-T/m^2]' ; '[micro-T/m^2]' ; '[micro-T/m^2]' ; '[micro-T/m^2]' ; '[micro-T/m^2]' ; } ;
 
 Shim.Com = [] ;  
+Shim.Dac = [] ;
 
 % channels 1-3 refer to the gradient offsets, 4-8 to the 2nd order shims
 Shim.Amp.nChannels       = 8 ;  
@@ -32,22 +33,65 @@ Shim.Amp.dynamicChannels = false( Shim.Amp.nActiveChannels, 1 ) ;
 
 
 % NB: One can use the Siemens commandline AdjValidate tool to get all the values below:
-% Shim.Amp.maxCurrentPerChannel
-%   First 3 terms correspond to the linear gradient offsets: [units: micro-T/m]
-%   Last 5 terms correspond to the 2nd order shims: [units: micro-T/m^2]
-%   These last 5 correspond to  9.998 A 
-%
-%   Though I'd prefer to have all the shim terms in units of A, I don't (yet)
-%   know what the corresponding values would be for the linears. Moreover,
-%   these "multipole units" are those displayed in the Syngo/3D Shim card,
-%   so, it may simply be more convenient to deal with the shim currents in
-%   "display units".
+% Shim.Amp.maxCurrentPerChannel 
 Shim.Amp.maxCurrentPerChannel = [ 2300, 2300, 2300, 4959.01, 3551.29, 3503.299, 3551.29, 3487.302 ]' ;
 
+end
+% =========================================================================
 
-Shim.Dac = [] ;
-% Shim.Dac.resolution = 16 ; % [bits]
-% Shim.Dac.maxCurrent = 5 ; % (absolute) [units: amps]
+end
+% =========================================================================
+% =========================================================================
+methods(Static)
+% =========================================================================
+function [ shimValues ] = converttomultipole( shimValues )
+%CONVERTTOMULTIPOLE
+% 
+% shimValues = CONVERTTOMULTIPOLE( shimValues )
+%
+% Shim values stored in MrProt (private Siemens DICOM.Hdr) are in units of 
+% DAC counts for the gradient offsets and in units of mA for the 2nd order shims.
+% CONVERTTOMULTIPOLE uses the information given by the Siemens commandline tool
+%   AdjValidate -shim -info
+% to convert a vector of shim settings in those units into the "multipole" values
+% which are used in the Siemens GUI display (i.e. Shim3d)
+
+nChannels = numel( shimValues ) ;
+
+MAX_SHIMS.mp  = [ 2300; 2300; 2300; 4959.01; 3551.29; 3503.299; 3551.29; 3487.302; ] ;
+MAX_SHIMS.dcm = [ 14436; 14265; 14045; 9998; 9998; 9998; 9998; 9998; ] ;
+
+if nChannels == 3 
+    % input shimValues are gradient offsets [units : DAC counts]
+    % output shimValues units : micro-T/m]
+    
+    shimValues = shimValues .* ( MAX_SHIMS.mp(1:3) ./ MAX_SHIMS.dcm(1:3) ) ;
+
+    if( any( abs( shimValues ) > MAX_SHIMS.mp(1:3) ) )
+        warning('Multipole values exceed known system limits. Check inputs and limits specified in ShimSpecs') ;
+    end
+
+elseif nChannels == 5
+    % input shimValues are for the 2nd order shims [units : mA]
+    % output shimValues units : micro-T/m^2]
+
+    shimValues = shimValues .* ( MAX_SHIMS.mp(4:8) ./ MAX_SHIMS.dcm(4:8) ) ;
+    
+    if( any( abs( shimValues ) > MAX_SHIMS.mp(4:8) ) )
+        warning('Multipole values exceed known system limits. Check inputs and limits specified in ShimSpecs') ;
+    end
+
+elseif nChannels == 8
+
+    shimValues = shimValues .* ( MAX_SHIMS.mp ./ MAX_SHIMS.dcm ) ;
+    
+    if( any( abs( shimValues ) > MAX_SHIMS.mp ) )
+        warning('Multipole values exceed known system limits. Check inputs and limits specified in ShimSpecs') ;
+    end
+
+else
+    error('Input shimValues vector should have 3, 5, or 8 elements, respectively corresponding to gradient terms, 2nd order shims, and both') ;
+end
 
 end
 % =========================================================================
@@ -57,5 +101,3 @@ end
 % =========================================================================
 
 end
-
-
