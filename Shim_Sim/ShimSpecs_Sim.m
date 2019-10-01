4697ecbdbceb1a9e5a30aff9ceae51bc23330cde
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2019-10-01 11:33:58 -0400

    ShimSpecs_Sim.m : update doc

--- Shim_Sim/ShimSpecs_Sim.m
+++ Shim_Sim/ShimSpecs_Sim.m
@@ -1,14 +1,22 @@
 classdef ShimSpecs_Sim < ShimSpecs
-%SHIMSPECS_DES
+%SHIMSPECS_SIM
 % 
-% Shim System Specifications for a simulated coil design, to be used in
-% conjunction with ShimOpt_Sim
+% Shim system specifications for a simulated coil design. 
+% 
+% Usage
+%
+%   Specs = ShimSpecs_Sim( Specs )
+%
+% **This class only serves to typecast a parameters struct defined in a ShimOpt_
+% class (e.g. ShimOpt_SphericalHarmonics, or ShimOpt_Sim) into a "ShimSpecs"
+% object given that certain functions may expect the latter as opposed to a
+% generic struct.**
 %
 % First used in Lopez Rios et al. Integrated AC / DC coil and dipole Tx array
 % for 7T MRI of the spinal cord. In: Proc. 27th Annu. Meet. ISMRM, Montreal,
 % Canada, 2019. Abstr. 0220.
 % 
-% ShimSpecs_Sim is a ShimSpecs subclass. See ShimSpecs documentation for usage.
+% ShimSpecs_Sim is a ShimSpecs subclass. For more info, see ShimSpecs.m
 %
 % =========================================================================
 % Author::ryan.topfer@polymtl.ca
@@ -29,6 +37,7 @@ Shim.Dac = [] ; % N/A
 
 Shim.Id.systemName            = Specs.systemName ;
 Shim.Id.channelNames          = Specs.channelNames ;
+Shim.Id.channelUnits          = Specs.channelUnits ;
 
 Shim.Amp.nChannels            = Specs.nChannels ;
 Shim.Amp.nActiveChannels      = Specs.nChannels ;
