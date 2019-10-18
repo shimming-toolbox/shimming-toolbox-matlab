3f737e1da1ad6d3ab22f333430ad8bcb34db798e
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2019-10-18 10:47:18 -0400

    demozshim draft (FieldEval.modelfield needs to be changed for the PMU as output 'Field' in demozshim does not look correct

--- example/demozshim.m
+++ example/demozshim.m
@@ -6,19 +6,24 @@
 % Author: ryan.topfer@polymtl.ca
 % =========================================================================
 
+% load t2w magnitude for SCT segmentation
+Mag = MaRdI( '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/05-T2w_1mm' ) ;
 
 %% -------
 % load images and physio recording as FieldEval + ProbeTracking objects :
 
 % paths to DICOM folders:
-pathToMag   = '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_72/07-realtime_fieldmap_deepBreath' ;
-pathToPhase = '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_72/08-realtime_fieldmap_deepBreath' ;
+pathToMag   = '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/08-realtime_fieldmap_normalBreath' ;
+pathToPhase = '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/09-realtime_fieldmap_normalBreath' ;
+
+% /Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/08-realtime_fieldmap_normalBreath
+% /Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/09-realtime_fieldmap_normalBreath
 
 % field map time series
 Fields = FieldEval( pathToMag, pathToPhase ) ; 
 
 % Siemens PMU recording
-Pmu   = ProbeTracking( '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_72/PMU/acdc_72_02.resp' ) ;
+Pmu   = ProbeTracking( '/Users/ryan/Projects/Shimming/Acdc/202004_Ismrm/data/acdc_73/Probes/acdc_73_01.resp' ) ;
 
 %% -------
 % link the two objects (interpolate PMU to the image times)
