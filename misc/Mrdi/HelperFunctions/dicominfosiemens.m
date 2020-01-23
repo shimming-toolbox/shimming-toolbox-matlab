d475b93eb1d85291ed9dc588a00a05804e37fe6d
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-01-23 18:31:59 -0500

    dicominfosiemens.m : Add option to input DICOM Hdr struct (i.e. as returned from dicominfo( ) )instead of the DICOM filename

--- misc/Mrdi/HelperFunctions/dicominfosiemens.m
+++ misc/Mrdi/HelperFunctions/dicominfosiemens.m
@@ -1,20 +1,40 @@
-function [ Hdr ] = dicominfosiemens( filename )
+function [ Hdr ] = dicominfosiemens( varargin )
 %DICOMINFOSIEMENS  Return standard DICOM Hdr struct with added fields from PARSE_SIEMENS_SHADOW(): .Img, .Ser, .MrProt 
 %
 % Hdr = DICOMINFOSIEMENS( filename ) 
+% Hdr = DICOMINFOSIEMENS( Hdr ) 
+% 
+% Input can be either the DICOM filename, or the corresponding Hdr struct as
+% returned by DICOMINFO( filename )
 %
 % See also DICOMINFO, PARSE_SIEMENS_SHADOW
+   
+    if ( nargin ~= 1 ) && isempty( varargin{1} )
+        error('Invalid input. See HELP dicominfosiemens') ;
+    end
+    
+    switch class( varargin{1} )
+        case {'char', 'string'}
+            Hdr = dicominfo( varargin{1} ) ;
+        case 'struct'
+            Hdr = varargin{1} ;
+        otherwise
+            error('Invalid input. See HELP dicominfosiemens') ;
+    end
 
-    Hdr = dicominfo( filename ) ;
+    if strcmp( Hdr.Manufacturer, 'SIEMENS' )
+        [ Hdr.Img, Hdr.Ser, Hdr.MrProt ] = parse_siemens_shadow( Hdr ) ;
+        fprintf('\n') ;
 
-    % parses additional info (e.g. "MrProt" (re: protocol))
-    [ Hdr.Img, Hdr.Ser, Hdr.MrProt ] = parse_siemens_shadow( Hdr ) ;
-    fprintf('\n') ;
-
-    % parse_mrprot produces many, many warning messages (at least for DICOMs from the Prisma).
-    % This should suppress all but the 1st instance:
-    [ lastMsg, lastWarnId ] = lastwarn( ) ;
-    warning( 'off', lastWarnId ) ;
+        % parse_mrprot produces many warnings (at least for DICOMs from the Prisma).
+        % This should suppress all but the 1st instance:
+        [ lastMsg, lastWarnId ] = lastwarn( ) ;
+        warning( 'off', lastWarnId ) ;
+    else
+        warning( ['Expected DICOM header field .Manufacturer == "SIEMENS".' ...
+           ' Aborting call to parse_siemens_shadow()'] ) ;
+        return ;
+    end
 
 end
 
