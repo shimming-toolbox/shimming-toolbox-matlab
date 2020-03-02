013bcba18ebdbcd85cf2e0cc38be9a96c6264a75
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-02 02:11:42 -0800

    cosmetic fixes

--- helpDocMd/src/@Documentor/documentbasic.m
+++ helpDocMd/src/@Documentor/documentbasic.m
@@ -11,13 +11,10 @@ Info = Dr.Info.Attributes ;
 
 assert( ~strcmp(Info.mType, "NA"), 'Invalid mFile' ) ;
 
-docStr    = strings( 6,1 ) ;
-docStr(1) = strcat( "# ", Info.Name, " #", " (a MATLAB(R) ", Info.mType, ")" ) ;
-docStr(2) = "" ;
-docStr(3) = strcat( "_", Info.Description, "_" ) ;
-docStr(4) = "" ;
-docStr(5) = "### Description ###" ;
-docStr    = [docStr ; ""; Info.DetailedDescription ; "" ] ;
+docStr = [ strcat( "# ", Info.Name ) ; "" ];
+docStr = [ docStr ; strcat(  "_", Info.Description, "_" ) ; "" ] ;
+docStr = [ docStr ;  strcat( "(a MATLAB(R) ", Info.mType, ") " ) ; "" ] ;
+docStr = [ docStr ; "### Description ###"; "" ; Info.DetailedDescription ; "" ] ;
 
 end 
 
