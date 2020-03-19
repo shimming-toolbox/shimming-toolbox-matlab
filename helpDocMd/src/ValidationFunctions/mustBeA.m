b13ac1e1929d00d720746fd9a09bd3c3dfec801b
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-19 08:07:51 -0400

    mustBeA.m: Truncate help entry

--- helpDocMd/src/ValidationFunctions/mustBeA.m
+++ helpDocMd/src/ValidationFunctions/mustBeA.m
@@ -1,18 +1,18 @@
-function [] = mustBeA( A, permittedTypes )
+function [] = mustBeA( A, allowedTypes )
 %MUSTBEA Validate variable data-type/class 
 % 
-%   [] = mustBeA( A, permittedTypes )
+%   [] = mustBeA( A, allowedTypes )
 %
-% Issues an error if the class/datatype of `A` is not included among those
-% listed in `permittedTypes`. 
+% Throws an error if the datatype (aka "class") of `A` is not included among
+% the specified `allowedTypes` (a string array of permitted classes).
 %
-% __NOTE__
-% `mustBeA` merely asserts that `ismember( class(A), permittedTypes )`
-% evaluates to `true`. However, since nested function calls are not allowed
-% for function argument validation (See 1.) `mustBeA` can be used instead. 
+% __DESCRIPTION__
+% `mustBeA` is equivalent to `assert( 1==ismember( class(A), allowedTypes ) );`
+% (which hardly deserves its own file) However, as nested function calls are not
+% permitted for argument [validation], this function can be used instead.
 %
 % __SEE__
-% 1. [Arg. validation](https://www.mathworks.com/help/matlab/matlab_prog/function-argument-validation-1.html)
+% 1. [validation](https://www.mathworks.com/help/matlab/matlab_prog/function-argument-validation-1.html)
 % 2. [class](https://www.mathworks.com/help/matlab/ref/class.html)
 % 3. [mustBeMember](https://www.mathworks.com/help/matlab/ref/mustbemember.html)
 % 4. [isa](https://www.mathworks.com/help/matlab/ref/isa.html)
@@ -20,12 +20,12 @@ function [] = mustBeA( A, permittedTypes )
 % SEE ALSO
 % class, mustBeMember, isa
 
-if ismember( class( A ), permittedTypes )
-    return;
+if ismember( class( A ), allowedTypes )
+    return 
 end
 
 error( 'mustBeA:invalidType', ['Input type/class must be == [' ...
-       char( "'" + strjoin( permittedTypes, "'|'") +"'") '].\n'], '%' ) ;  ;
+       char( "'" + strjoin( allowedTypes, "'|'") +"'") '].\n'], '%' ) ;  ;
 
 end
 
