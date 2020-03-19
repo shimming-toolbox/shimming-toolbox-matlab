642e566dbce136e446f541b7a4929b66e988bd22
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-19 13:46:12 -0400

    abspath.m : Return empty strings for invalid paths instead of throwing an error

--- helpDocMd/src/abspath.m
+++ helpDocMd/src/abspath.m
@@ -1,30 +1,26 @@
-function [ pathOut, pathType ] = abspath( pathIn, isAbs )
+function [ pathOut, pathType ] = abspath( pathIn )
 %ABSPATH Validate and convert file system paths
 %    
 %    [pathOut, pathType] = abspath( pathIn )  
 % 
-% ABSPATH checks each element of the input argument `pathIn` for valid file
-% system paths (which can be relative or full/absolute) and returns two arrays
-% of the same type (string, char, or cellstr) and size (arbitrary) as the
-% input.
+% `abspath` wraps to the Matlab function [fileattrib] to check each element of
+% the input argument `pathIn` for valid file system paths (which can be
+% relative or full/absolute). It returns two arrays of the same type (string,
+% char, or cellstr) and size (arbitrary) as the input.
 %
 % The possibilities for a given return element `(i)` are:
 % 
-%|`pathIn(i)`  | `pathOut(i)`| `pathType(i)`                       |
-%|:------------|:-----------:| :---------------------------------: |
-%| is a file   | full path   | "file"                              |
-%| is a folder | full path   | "directory"                         |
-%| else        |   ""        | the error message from [fileattrib] |
+% |`pathIn(i)`  | `pathOut(i)`| `pathType(i)`                       |
+% |-------------|-------------|-------------------------------------|
+% | is a file   | full path   | "file"                              |
+% | is a folder | full path   | "directory"                         |
+% | else        |   ""        | the error message from [fileattrib] |
 %
 % __Note__ 
-% MATLAB function [fileattrib] is the ~proverbial workhorse~--naaayy--
-% *functional!* workhorse behind abspath, which is really just a wrapper
-% function, otherwise dedicated to housekeeping. 
-% 
 % Re: invalid input paths, the error message returned from fileattrib() will
 % presumably be 'No such file or directory.' However the MATLAB documentation
-% is unclear as to whether other possibilities exist (i.e. in the case where
-% the function is called with a single argument). 
+% is unclear as to whether other possibilities exist for the case where
+% the function is called with a single argument, as is done here. 
 %
 % For more info, refer to the MATLAB documentation.
 %
