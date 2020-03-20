de12a95fe4670afb75cc266a99a9cf4e3965b27f
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-20 08:14:49 -0400

    mfiletype.m clarify docs

--- helpDocMd/src/@Informer/mfiletype.m
+++ helpDocMd/src/@Informer/mfiletype.m
@@ -5,44 +5,45 @@ function [mType, mPath, mExist] = mfiletype( mFile )
 %
 % __DESCRIPTION__
 % `mfiletype` checks the list of Matlab source files specified by the [string-,
-% char-, or cellstr-] array of file paths `mFile` and returns the string vector `mType`
-% with the i-th element containing
+% char-, or cellstr-] array of file paths `mFile` and returns:
+% 
+% 1. the string vector `mType` with entries:
 %
-% `mType(i)`| `mFile(i)` is...
+% `mType(i)`| When input `mFile(i)` is...  
 % ----------|-----------------------------------------------------------------|
-% "script"  | a script file
+% "script"  | a script file 
 % "function"| a function file (even one with void arguments)
 % "classdef"| a class definition file
 % "method"  | a class method file (any .m file in a folder beginning with "@")
 %   "NA"    | an unimplemented or non-Matlab file with a .m file extension
 %    ""     | an invalid file path (e.g. folder, non-Matlab, or non-existent)
+% 
+% 2.`mPath` a string vector of the full file paths corresponding to the input
 %
-% **Note** `mType(i)` will be "NA" whenever the file could not be assessed
-% (e.g. a function that produces an error when called by `nargin`). An
-% assignment other than "NA" however does not guarantee that a file *is*
-% implemented, and it may still contain errors.
+% 3. `mExist` a vector of return from calling `mExist(i)=exist(mFile(i));` i.e.
+% with the single file path argument, so, it *should* be == 2, however...
 %
-% Additional returns: 
-%
-% -`mPath` a string vector containing the full file paths to the source files
-%
-% -`mExist` a vector of doubles returned by calling the Matlab [exist] function
-% for each of the files. Note that the call is made with the single path
-% argument, so `mExist` will be "2" even for classdef files.  Also, note that
-% `exist()` will return "2" even when the file merely ends in '.m' but is not
-% actually a MATLAB file.
+% __NOTE__
+% The current implementation, which follows the method outlined [here], is
+% very rudimentary and relies entirely on Matlab's [exist] function, which, as
+% the official documentation states, *does not* check the contents of '.m'
+% files. So, although `mType(i)` will be "NA" whenever the file could not be
+% assessed (e.g. a function that produces an error when called by `nargin`), an
+% assignment other than "NA" however does not guarantee that a file *is* in
+% working condition: it may still contain errors.
 %
 % __SEE__
-% - [exist][https://www.mathworks.com/help/matlab/ref/exist.html]
-%
-% `mfiletype` implements the method outlined
-% [here](https://blogs.mathworks.com/loren/2013/08/26/what-kind-of-matlab-file-is-this/)
+% [here]: https://blogs.mathworks.com/loren/2013/08/26/what-kind-of-matlab-file-is-this/
+% [exist]: https://www.mathworks.com/help/matlab/ref/exist.html/
 %
 % See also
 % EXIST
     arguments
         mFile { mustBeA( mFile, ["string" "char" "cellstr"] ) } ;
     end
+
+% pos. TODO? Enable optional keyword input (e.g. function name) and defer to it
+% if abspath etc. fail to find anything assuming file path paths? ("+")
 
 mPath  = abspath( strip( string( mFile ) ) ) ; 
 mExist = arrayfun( @exist, mPath ) ; 
@@ -52,15 +53,16 @@ mType( ~endsWith( mPath, ".m" ) ) = "NA" ;
 % mPath indices corresponding to .m files
 iMFiles = find( [ isfile( mPath ) & endsWith( mPath, ".m" ) ] ) ;
 
-% cd to each file to ensure it has precedence on the Matlab path, avoiding
-% naming conflicts. Alt., the MATLAB path could be updated for each file, then
-% reverted, but that may be slower.
+% Note: exist( mPath(i) , "class" ) will returns 0 even when mPath(i) is a
+% class file. Instead, one needs to use the class *name*. To hopefully bypass
+% path precedence and namespace issues: cd to each file to ensure it has
+% precedence on the Matlab path, avoiding naming conflicts. (Alt., the MATLAB
+% path could be updated for each file, then reverted, but that may be slower.)
 userDir = pwd ;
 
 for iM = 1 : nnz( iMFiles ) 
    
     [ folder, name, ext ] = fileparts( mPath( iMFiles( iM ) ) ) ;
-    
     cd( folder ) ;  
 
     % Explicitly test classdef possibility first to bypass the other statements.
