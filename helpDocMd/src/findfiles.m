76548c7e4f8ae289806ad8f9364c484c0a8779da
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-22 02:20:49 -0400

    findfiles.m ! (BREAK) change order of returns, refactor, add option to return paths to folders as well/instead of files

--- helpDocMd/src/findfiles.m
+++ helpDocMd/src/findfiles.m
@@ -1,19 +1,19 @@
-function [paths, List] = findfiles( sFolder, sFilePattern, isRecursive, isExcludingHidden )
+function [paths, List] = findfiles( sFolder, sPattern, isRecursive, isExcludingHidden, returnType )
 % FINDFILES Search a directory for filenames matching a pattern
+%     
+%    [paths, List] = findfiles( sFolder, sPattern, isRecursive, isExcludingHidden, returnType )
 % 
-%    [paths, List] = findfiles( sFolder, sFilePattern, isRecursive, isExcludingHidden )
+% Looks for files and/or subfolders in `sFolder` with names matching `sPattern`
+% by calling Matlab function [dir] and returns the file paths as elements of a
+% string column vector `paths`. The single-element structs output by `dir()`
+% are arrayed and returned as `List.
 % 
-% Looks for files in `sFolder` with filenames matching `sFilePattern` by
-% calling Matlab function [dir] and returns the file paths as elements of a
-% string column vector `paths`. Structs output by `dir()` are arrayed and
-% returned as `List.
-% 
-% INPUTS
+% __INPUTS__ 
 %     
 %   sFolder=["."]
 %     The base directory of the search.    
 %
-%   sFilePattern=["*.*"] 
+%   sPattern=["*.*"] 
 %     The searchPattern of interest. If provided as a string array, patterns
 %     are searched successively. (The default corresponds to including  all
 %     files with explicit with explicit file extensions.)
@@ -23,8 +23,12 @@ function [paths, List] = findfiles( sFolder, sFilePattern, isRecursive, isExclud
 %
 %   isExcludingHidden=[true|1]
 %     Toggle to include (1) or exclude (0) hidden files (i.e. for
-%     Unix: filenames beginning with "." )  
+%     Unix: filenames beginning with ".")
 %
+%   returnType=["files"]
+%     Selects what type of path elements are retained in the two outputs : 
+%     Options are: "files", "folders", or "both". 
+% 
 % ETC 
 %
 %   For more info, refer to the documentation for
@@ -33,39 +37,64 @@ function [paths, List] = findfiles( sFolder, sFilePattern, isRecursive, isExclud
 % See also DIR
     arguments
         sFolder(1,:) { mustBeStringScalarOrCharVector, mustBeFolder } = "." ;
-        sFilePattern  {mustBeStringOrCharOrCellstr} = "*.*" ;    
+        sPattern  {mustBeStringOrCharOrCellstr} = "*.*" ;    
         isRecursive(1,1) {mustBeBoolean} = true ;
         isExcludingHidden(1,1) {mustBeBoolean} = true ;
+        returnType(1,:) { mustBeStringScalarOrCharVector, ...
+            mustBeMember( returnType, ["files" "folders" "both"] ) } = "files" ;
     end
 
-searchStr = strcat( sFolder, filesep )  ;
+%% Cast as string
+sPattern  = string( sPattern ) ;
+searchStr = string( fullfile( sFolder, filesep ) ) ;
 
 if isRecursive
-    searchStr = strcat( searchStr, '**', filesep )  ;
+    searchStr = fullfile( searchStr, "**", filesep )  ;
 end
 
-sFilePattern = string( searchPattern ) ;
+%% Search
+List = dir( strcat(searchStr, sPattern(1) ) ) ;
 
-List = dir( strcat(searchStr,sFilePattern(1)) ) ;
-
-for iPattern = 2 : numel( sFilePattern )
-    List = [ List ; dir( strcat( searchStr, sFilePattern(iPattern) ) ) ] ;
+for iPattern = 2 : numel( sPattern )
+    List = [ List ; dir( strcat( searchStr, sPattern(iPattern) ) ) ] ;
 end
 
-%% remove folders
-List = List( ~[ List(:).isdir ] ) ;
+paths = fullfile( string( {List(:).folder} )', string( {List(:).name} )' ) ;
 
-%% remove 'hidden' files
 if isExcludingHidden 
-    iHidden = startsWith( string( { List(:).name } ), "." ) ;
-    List    = List( ~iHidden ) ;
+    if isunix
+        iHidden = contains( paths, filesep + "." ) ;
+    else 
+        [~, Attributes] = arrayfun( @fileattrib, paths ) ;
+        iHiddenFolders  = [ Attributes.hidden(:) & Attributes(:).directory ] ;
+        iHiddenFiles    = [ Attributes.hidden(:) & ~Attributes(:).directory ] ;
+        iInclude        = ~[ iHiddenFiles | iHiddenFolders ] ; 
+        % ^redundant if 'hidden' is attributed recursively. not using a PC so idk.)
+    end
+    iInclude = ~iHidden ;
+else
+    iInclude = true( size( paths ) ) ; 
 end
 
-paths = "" ;
-for iFile = 1 : length( List ) 
-    paths( iFile ) = string( fullfile( List(iFile).folder, List(iFile).name ) ) ;
+switch returnType
+    case "files"
+        iInclude = [ iInclude & ~[ List(:).isdir ]' ] ;
+    case "folders"
+        iInclude = [ iInclude & [ List(:).isdir ]' ] ; 
+    % otherwise % (both)
 end
 
-paths = paths' ;
+List  = List( iInclude ) ;
+paths = paths( iInclude ) ;
+
+%% Recast to input type
+switch class( sFolder )
+    case 'char'
+        paths = char( paths ) ;
+    case 'cell'
+        paths = cellstr( paths ) ;
+    otherwise
+        paths = string( paths ) ; % should already be true
+end
 
 end
