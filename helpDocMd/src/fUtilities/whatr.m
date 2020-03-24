e980ded6ce539c81b56b85b8801631d3b8a6e3c3
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-03-24 02:29:49 -0400

    Tidy user doc

--- helpDocMd/src/fUtilities/whatr.m
+++ helpDocMd/src/fUtilities/whatr.m
@@ -1,30 +1,28 @@
 function [ Info ] = whatr( baseDir, isExcludingHidden )
-%WHATR Returns struct array from recursive calls to WHAT across subdirectories
+% WHATR Recursive` what()` 
+%     
+%     Info = whatr( baseDir, isExcludingHidden )
 %
-% WHATR denotes _recursive_ WHAT ("what -r"?)
-% 
-% ### Usage ###
-% 
-% Info = WHATR( baseDir, isExcludingHidden )
+%  Calls MATLAB function [what] for `baseDir` and each of its subdirectories,
+%  and arrays the results to return `Info`.
 %
-% #### Inputs ####
-% 
-% baseDirectory 
+% __INPUTS__ 
+%     
+%   `baseDir=["."]`
+%     The parent/top directory path as a string scalar or character vector.
 %
-% - parent/top directory path as a string scalar or character vector.
-%   [default: "." ]
+%   `isExcludingHidden=[true|1]`
+%     A Boolean toggle: when set to false, hidden folders are included in the returned list.
 %
-% - isExcludingHidden     
-%   [default = 1]
-%   Boolean toggle includes hidden folders in the returned list when set to 0 (false).
+% __ETC__ 
 %
-% ### References ###
+% To help remember the function name, think "what -r" on the commandline, and
+% "what files are there?" in English.
 %
-% See also:
+% - [what](https://www.mathworks.com/help/matlab/ref/what.html)
 %
-% - [mapdirectorytree](local link?) 
-%
-% - <https://www.mathworks.com/help/matlab/ref/what.html>
+% See also
+% WHAT
     arguments
         baseDir(1,:) { mustBeStringScalarOrCharVector, mustBeFolder } = "." ;
         isExcludingHidden(1,1) { mustBeBoolean } = true ;
