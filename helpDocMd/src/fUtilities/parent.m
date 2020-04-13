37ad13eafd0fea0990953602e96465fe13ee0303
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-04-12 23:17:56 -0400

    Change input to accept a string specifiying the return directory as 'common' (shared base parent) or 'single' (single level parent)

--- helpDocMd/src/fUtilities/parent.m
+++ helpDocMd/src/fUtilities/parent.m
@@ -1,43 +1,33 @@
-function [parentDir, baseDir] = parent( pathIn )
+function [parentDir] = parent( pathIn, level )
 % PARENT Return immediate and common (ap-)parent directories 
 %    
-%    [ parentDir, baseDir ] = parent( pathIn ) ;
+%    [ parentDir ] = parent( pathIn, 'single' ) ;
+%    [ parentDir ] = parent( pathIn, 'common' ) ;
 %
-% Wraps to Matlab function [fileparts] to return the immediate (single level)
-% parent folder(s) of `pathIn` as `parentDir`. If `pathIn` contains multiple
-% paths, then the path to the first common base folder (the "fork") is returned
-% as `baseDir`.
-% 
+% When called with a single argument, or with the second argument 'single',
+% `parent` wraps to Matlab function [fileparts] to return the immediate (single level)
+% parent folder(s) of `pathIn` as `parentDir`. 
+%
+% With a second argument of 'common', the unique path to the common base
+% directory among `pathIn` (i.e. the "fork") is returned. If the input paths
+% have nothing in common (even the leading character), then baseDir is assigned
+% '?'. 
+%
+% If `pathIn` consists of a single entry the result will be the same in both cases.
+%
 % **NOTE** Like `fileparts`, the input does not need to refer to existing
 % file system paths (consider the mnemonic "ap-*parent*" for this function).
 % Unlike `fileparts`, the input to `parent` can contain multiple path elements.
-% 
-% __INPUTS__
-%     
-%   pathIn
-%     A string-, char-, or cellstr-array of paths.
 %
-% __OUTPUTS__
-%
-%   parentDir
-%     Paths to the immediate parent folders of the elements of `pathIn`, formed
-%     by calling `fileparts( )` for each element.
-%  
-%   baseDir
-%     The first common parent directory the elements of `pathIn`. (The
-%     "fork").  If pathIn consists of a single path, `baseDir` is the same as
-%     `parentDir`. If the input paths have nothing in common (even the leading
-%     character), then baseDir is assigned '?'.
-%
-% __Example 1__
+% __EXAMPLE__
 % ```
 % % pathIn is a triple-row string vector
 % >> pathIn = [ "/A/B/C" ; "/A/B/C2/D2/E2" ; "/A/B/C3/D3/E3" ; ] ;
 % 
-% % omiting the terminating semi-colon for display produces:
-% >> [parentDir, baseDir ] = parent( pathIn )
+% % Omiting the terminating semi-colon for display:
+% >> parent( pathIn )
 %
-% parentDir =
+% ans =
 %
 %   3x1 string array
 %
@@ -45,7 +35,9 @@ function [parentDir, baseDir] = parent( pathIn )
 %     "/A/B/C2/D2"
 %     "/A/B/C3/D3"
 %
-% baseDir =
+% >> parent( pathIn, 'common' )
+%
+% ans =
 %
 %     "/A/B"
 %
@@ -57,10 +49,15 @@ function [parentDir, baseDir] = parent( pathIn )
 % FILEPARTS
     arguments
         pathIn {mustBeStringOrCharOrCellstr}
+        level {mustBeMember(level,["single" "common"])} = "single" ;
     end
 
-P         = Pathologist( pathIn ) ;
-parentDir = P.parentDir ;
-baseDir   = P.baseDir ;
+P = Pathologist( pathIn ) ;
+
+if strcmp( level, 'single' ) || ( P.nPaths == 1 )
+    parentDir = P.parentDir ;
+else
+    parentDir = P.baseDir ;
+end
 
 end
