518ddb4181902908049b4780df7861003507c69e
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-05-21 14:59:38 -0400

    NEW: read_nii.m — read nifti image, header, and load .json when present

---
+++ Img/read_nii.m
@@ -0,0 +1,37 @@
+function [img,info,json] = read_nii( niiFile )
+%READ_NII Load NIfTI image and header; and, when present, the accompanying .json sidecar
+%     
+%     [img, info]       = read_nii( niiFile )
+%     [img, info, json] = read_nii( niiFile )
+% 
+% The input `niiFile` is the path to the NIfTI image as a string scalar or
+% character vector. When called with 2 output arguments, the function is
+% equivalent short-hand for
+%     
+%     info = niftiinfo( niiFile ); img = niftiread( info ) 
+%
+% When called with the 3rd output argument, the function checks the parent
+% folder of `niiFile` for an identically named file but with a .json file extension.
+% When such a file is present, the 3rd output is returned as a struct via 
+% `json = jsondecode( fileread( jsonFile ) );` otherwise, `json = []`.
+arguments
+    niiFile(1,:) string {mustBeStringScalarOrCharVector, mustBeFile} ;
+end
+
+info = niftiinfo( niiFile ); 
+img  = niftiread( info ); 
+
+if nargout < 3
+    return;
+end
+
+[folder, name] = fileparts( niiFile );
+jsonFile       = fullfile( folder, name + ".json" ); 
+
+if isfile( jsonFile )
+    json = jsondecode( fileread( jsonFile ) );     
+else
+    json = [];
+end
+
+end
