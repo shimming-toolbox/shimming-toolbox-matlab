981f5837acfcdbd0cac2b640ca6d401bef603d76
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-05-22 14:23:55 -0400

    Added if for windows compatibility

--- videotape-struts-giraffe/dicom_to_nifti.m
+++ videotape-struts-giraffe/dicom_to_nifti.m
@@ -7,8 +7,14 @@ disp(unsortedDicomDir);
 disp(pathNifti);
 
 % BEWARE: shell injection attacks here
-if system(['which dcm2niix']) == 1
-  error 'dcm2niix is not installed.'
+if ispc == 1
+    if system(['where dcm2niix']) == 1
+        error 'dcm2niix is not installed.'
+    end
+else
+    if system(['which dcm2niix']) == 1
+        error 'dcm2niix is not installed.'
+    end
 end
 participant = '';
 system(['dcm2bids -d "' unsortedDicomDir '"' ' -o '  '"' pathNifti '"' ' -p '  '"' participant '"' ' -c '  'config.json']);
