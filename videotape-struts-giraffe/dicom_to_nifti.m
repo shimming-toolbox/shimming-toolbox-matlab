2b917b291bb7fad1750445955ebc196c35cde273
Alexandre D'Astous <po09i@hotmail.com>
2020-05-22 01:45:01 -0400

    Same nifti folder structure as nifti directory

--- videotape-struts-giraffe/dicom_to_nifti.m
+++ videotape-struts-giraffe/dicom_to_nifti.m
@@ -1,17 +1,22 @@
-function dicom_to_nifti( unsortedDicomDir, pathNifti )
+function dicom_to_nifti( sortedDicomDir, pathNifti )
 
 %TODO: Output single multi-echo series as 4d nifti
 
 mkdir(pathNifti);
-disp(unsortedDicomDir);
-disp(unsortedDicomDir);
-disp(unsortedDicomDir);
+disp(sortedDicomDir);
 disp(pathNifti);
-% BEWARE: shell injection attacks here
-if system(['which dcm2niix']) == 1
-  error 'dcm2niix is not installed.'
+
+folders = dir(sortedDicomDir);
+for iFolder = 3:length(folders)
+    % BEWARE: shell injection attacks here
+    if system(['which dcm2niix']) == 1
+      error 'dcm2niix is not installed.'
+    end
+    subFolderNifti = fullfile(pathNifti, folders(iFolder).name)
+    mkdir(subFolderNifti);
+    system(['dcm2niix -b y -a y -o ' subFolderNifti ' ' sortedDicomDir]);
+    
 end
-system(['dcm2niix -o ' pathNifti ' ' unsortedDicomDir]);
 
 end
 
