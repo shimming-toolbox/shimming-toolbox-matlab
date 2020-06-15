ce9a8b4dcdcc747d19c9467b9900f7406c642732
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-06-15 15:01:09 -0400

    NEW: +parts classes to form Shim Specs config: Channel.m and Port.m

---
+++ +b0shim/+parts/Channel.m
@@ -0,0 +1,77 @@
+classdef Channel 
+%b0shim.parts.Channel Defines a shim coil element
+% 
+% `Channel` provides a template description for a shim coil channel. 
+% An array of `Channel` objects is used to define a shim coil array.
+  
+
+properties
+
+    % Shim channel ID as a string-scalar.
+    %
+    % `name` defines the row names when tabulating optimization results 
+    % in `b0shim.Opt.optimizeshimcurrents` (i.e. for print display).
+    %
+    % __EXAMPLE__ 
+    % 
+    % % Generically, this could simply be  
+    % `name = "Ch. 1"`; 
+    %
+    % % Or, for a 2nd-order spherical harmonic term  
+    % `name = "XY (B22)"`; 
+    name(1,1) string  = "Ch" ;
+    
+    % Shim channel physical units as a string-scalar.
+    %
+    % `units` is used when tabulating optimization results in
+    % `b0shim.Opt.optimizeshimcurrents` (i.e. for print display).
+    %
+    % __EXAMPLE__ 
+    % 
+    % % Units are often expressed in Amperes  
+    % `units = "A"`;
+    %
+    % % Alternatively, for a 2nd-order spherical harmonic shim, units could be
+    % `units = "mT/m^2"`;
+    units(1,1) string = "A" ;
+    
+    % Range of permitted values as 2-element vector.
+    % 
+    % `limits` defines the default constraints during shim optimization:
+    % `limits(1)` specifies the minimum value; 
+    % `limits(2)` specifies the maximum value.
+    % 
+    % Both elements should be given in the corresponding `units`.
+    %
+    % __EXAMPLE__
+    % 
+    % For an "AC/DC" multi-coil array, with `units="A"`, the value might be 
+    % `limits = [-2.5 2.5];`. 
+    limits(1,2) {mustBeNumeric,mustBeReal} ; 
+
+    % Label specifying coil positioning mode as a string-scalar.
+    % 
+    % Options for `positioning` are:  
+    %
+    % - "iso-fixed": The coil is immobile relative to isocenter (e.g. a gradient coil)
+    %
+    % - "table-fixed": The coil position changes with the patient-table (e.g. spine shim array)
+    % 
+    % - "": Otherwise (e.g. coils can be freely rearranged)
+    positioning(1,1) string {mustBeMember(positioning,["iso-fixed" "table-fixed" ""])} = "iso-fixed";
+end
+
+% =========================================================================
+% =========================================================================
+methods 
+% =========================================================================
+function self = Channel( props )
+    return
+end
+% =========================================================================
+
+end
+% =========================================================================
+% =========================================================================
+
+end
---
+++ +b0shim/+parts/Port.m
@@ -0,0 +1,65 @@
+classdef Port 
+%b0shim.parts.Port Serial port protocol parameters
+% 
+% `Port` properties are simply 6 of the 7 the parameters passed to MATLAB's
+% `serialport` function when opening the communication line between a PC and
+% the microcontroller directing the shim amplifiers. Hence, the properties are
+% only relevant for hardware systems permitting direct control via MATLAB—USB
+% (i.e. systems that subclass `b0shim.Com` to define a communication interface).
+% For a host MRI, or a virtual shim array, this classdef is likely irrelevant.
+%
+% Properties:
+% ```
+% BaudRate, DataBits, StopBits, FlowControl, ByteOrder, Timeout
+% ```
+%
+% __NOTES__
+%
+% 1. While `Port` properties correspond to `serialport` arguments, the default
+% assignments differ. (`Port` defaults are based on the microcontrollers in use
+% at our lab.)
+%
+% 2. As a safety feature, the mandatory first argument to `serialport()`—the
+% device ID—is not included as a `Port` property: when connecting to a device,
+% a user still needs to specify the port name. (The parameter, in any case,
+% tends to vary as device names are determined by the OS.)
+%
+% See also  
+% serialport    https://www.mathworks.com/help/matlab/ref/serialport.html
+
+properties
+
+    % 2nd argument to serialport
+    BaudRate(1,1) uint32 {mustBeInteger,mustBePositive} = 9600;
+
+    % Name-value argument to serialport
+    DataBits(1,1) uint8 {mustBeMember(DataBits, [8 7 6 5] )} = 8;
+
+    % Name-value argument to serialport
+    StopBits(1,1) single {mustBeMember(StopBits, [1 1.5 2] )} = 1;
+
+    % Name-value argument to serialport
+    FlowControl(1,1) string {mustBeMember(FlowControl, ["none" "hardware" "software"] )} = "none";
+
+    % Name-value argument to serialport
+    ByteOrder(1,1) string {mustBeMember(ByteOrder, ["little-endian" "big-endian"] )} = "big-endian";
+
+    % Name-value argument to serialport
+    Timeout(1,1) single {mustBeNumeric, mustBePositive} = 10;
+    
+end
+
+% =========================================================================
+% =========================================================================
+methods 
+% =========================================================================
+function self = Port( )
+    return
+end
+% =========================================================================
+
+end
+% =========================================================================
+% =========================================================================
+
+end
