1849aa4df421b5336bc4c9d244d708a88a20565c
Alexandre D'Astous <po09i@hotmail.com>
2020-06-15 19:11:06 -0400

    loadniftis.m : Update description

--- videotape-struts-giraffe/loadniftis.m
+++ videotape-struts-giraffe/loadniftis.m
@@ -1,10 +1,17 @@
 function [niftis, info, json] = loadniftis(path)
-%loadniftis Load a nifti acquisition from dcm2bids 
+%LOADNIFTIS Load a nifti acquisition from dcm2bids 
 %
-%     niftis = loadniftis(path)
+%     [niftis, info, json] = loadniftis(path)
 %
 % If `path` is a folder containing niftis, directly output niftis. If `path` is
 % a folder containing acquisitions, ask the user for which acquisition.
+% 
+% The output `niftis` is a 5d matlab vector stored according to (x,y,z,repetitions,echo) 
+% The output `info` and `json` are the corresponding header information
+% stored as (echoes)
+%
+% TODO : Currently, not all files will work, need to be more thorough when
+% looking for echoes, acquistions, and channels.
 
 if ~isfolder(path)
     error 'Path does not exist'
