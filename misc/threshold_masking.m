b739fe66a158c51e4def2e3d32397da8e3ea7b37
Eva <eva.alonso.ortiz@gmail.com>
2020-06-19 13:41:48 -0400

    changed signal threshold in threshold_masking.m

--- misc/threshold_masking.m
+++ misc/threshold_masking.m
@@ -3,7 +3,7 @@ function mask = threshold_masking(mag_data, sigma)
 mask = zeros(size(mag_data));
 mask = reshape(mask,size(mag_data,1)*size(mag_data,2)*size(mag_data,3)*size(mag_data,4),1);
 data = reshape(mag_data,size(mag_data,1)*size(mag_data,2)*size(mag_data,3)*size(mag_data,4),1);
-mask(find(data>100*sigma(1))) = 1;
+mask(find(data>30*sigma(1))) = 1;
 mask = reshape(mask,size(mag_data,1),size(mag_data,2),size(mag_data,3),size(mag_data,4));
 mask = mask(:,:,:,1,1);
 
