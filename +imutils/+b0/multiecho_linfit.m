e2c1dbf044234d5e93a1e2f438b25c6b3ca986e4
Eva <eva.alonso.ortiz@gmail.com>
2020-06-22 14:40:38 -0400

    changed variable names to camelCase in multiecho_linfit.

--- +imutils/+b0/multiecho_linfit.m
+++ +imutils/+b0/multiecho_linfit.m
@@ -1,17 +1,17 @@
-function [delf] = multiecho_linfit(compl_vol, ph_json)
+function [delf] = multiecho_linfit(complVol, ph_json)
 % multiecho_linfit Computes B0 fieldmaps based on a least-squares
 % fitting of the phase evolution with respect to time
 %
 % _SYNTAX_
 % 
-% [delf] = multiecho_linfit(compl_vol, ph_json)
+% [delf] = multiecho_linfit(complVol, ph_json)
 %
 % _DESCRIPTION_
 %
 % _INPUT ARGUMENTS_
 %
-%    compl_vol
-%      complex 4D data set compl_vol(x,y,z,t)
+%    complVol
+%      complex 4D data set complVol(x,y,z,t)
 %    ph_json
 %      phase json sidecar struct
 %
@@ -23,11 +23,11 @@ function [delf] = multiecho_linfit(compl_vol, ph_json)
 % Code adapted from https://github.com/evaalonsoortiz/LLSf_B0mapping
 
 % create magnitude and phase data volumes
-mag_data = abs(compl_vol);
-ph_data = angle(compl_vol);
+magData = abs(complVol);
+phData = angle(complVol);
 
 % get number of echoes and echo times
-numTE = size(mag_data,4); 
+numTE = size(magData,4); 
 delt = [ph_json.EchoTime];
 
 if ~isrow(delt) 
@@ -39,21 +39,21 @@ if numTE ~= size(delt,2)
 end
 
 % pre-allocate memory for variables
-delf = zeros(size(ph_data,1),size(ph_data,2),size(ph_data,3));
+delf = zeros(size(phData,1),size(phData,2),size(phData,3));
 
 % generate a mask 
-sigma = bkgrnd_noise(mag_data);
-mask = threshold_masking(mag_data, sigma);
+sigma = bkgrnd_noise(magData);
+mask = threshold_masking(magData, sigma);
 
 %-------------------------------------------------------------------------%
 % temporally unwrap phase across echoes
 %-------------------------------------------------------------------------%
-delPhaseNet(:,:,:,1) = ph_data(:,:,:,1);
+delPhaseNet(:,:,:,1) = phData(:,:,:,1);
 
 for n=2:numTE
 
-    vol_tmp0 = compl_vol(:,:,:,n-1);
-    vol_tmp1 = compl_vol(:,:,:,n);                       
+    vol_tmp0 = complVol(:,:,:,n-1);
+    vol_tmp1 = complVol(:,:,:,n);                       
 
     delPhase = angle( vol_tmp1.*conj(vol_tmp0) );
     delPhaseNet(:,:,:,n) = delPhaseNet(:,:,:,n-1) + delPhase;
@@ -65,15 +65,15 @@ end
 % frequency shift calculation (field map)
 %-------------------------------------------------------------------------%
 
-for i=1:size(ph_data,1)
-    for j=1:size(ph_data,2)
-        for k = 1:size(ph_data,3)
+for i=1:size(phData,1)
+    for j=1:size(phData,2)
+        for k = 1:size(phData,3)
             if (mask(i,j,k) == 0)
                 delf(i,j,k) = 0;
             else
                 A =  [delt(1:numTE)',ones(size(delt(1:numTE)'))];
                 B = squeeze(delPhaseNet(i,j,k,1:numTE));
-                w = squeeze(mag_data(i,j,k,1:numTE)).^2;
+                w = squeeze(magData(i,j,k,1:numTE)).^2;
                 [fit_para_tmp] = lscov(A,B,w);
                 delf(i,j,k) = fit_para_tmp(1)/(2*pi);
             end
