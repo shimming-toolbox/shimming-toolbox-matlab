0beada0ec75edbb191bac3db0f36a605eb63f036
Eva <eva.alonso.ortiz@gmail.com>
2020-06-22 10:51:34 -0400

    remove hard-coded signal threshold in threshold_masking.m

--- misc/threshold_masking.m
+++ misc/threshold_masking.m
@@ -1,9 +1,17 @@
-function mask = threshold_masking(mag_data, sigma)
+function mask = threshold_masking(mag_data, sigma, varargin)
+
+DEFAULT_SIGNAL_THRESHOLD = 30;
+
+if nargin > 2 
+    signal_threshold = varargin{1};
+else 
+    signal_threshold = DEFAULT_SIGNAL_THRESHOLD;
+end
 
 mask = zeros(size(mag_data));
 mask = reshape(mask,size(mag_data,1)*size(mag_data,2)*size(mag_data,3)*size(mag_data,4),1);
 data = reshape(mag_data,size(mag_data,1)*size(mag_data,2)*size(mag_data,3)*size(mag_data,4),1);
-mask(find(data>30*sigma(1))) = 1;
+mask(find(data>signal_threshold*sigma(1))) = 1;
 mask = reshape(mask,size(mag_data,1),size(mag_data,2),size(mag_data,3),size(mag_data,4));
 mask = mask(:,:,:,1,1);
 
