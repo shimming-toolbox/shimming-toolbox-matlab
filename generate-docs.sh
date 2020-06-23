9e7979581d3411f22f083db171b5cc03f63f8f59
Alexandre D'Astous <po09i@hotmail.com>
2020-06-23 16:50:40 -0400

    Added python -m as per website recommendations

--- generate-docs.sh
+++ generate-docs.sh
@@ -27,7 +27,7 @@ cd s
 
 # we need to do this in an explicit venv since our build agent
 # isn't fancy enough to spawn fresh containers/VMs for us
-pip install --user virtualenv
+python -m pip install --user virtualenv
 echo 1
 VENV=$(mktemp -d)
 echo 2
