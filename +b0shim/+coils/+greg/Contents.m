f6bfa8496f70b1ed8c49252f6e32a92ec6db86be
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-07-01 19:01:44 -0700

    DOC: Update +b0shim/+coils/+greg/Contents.m with more interesting stuff (tryingto embed slides for the website *Note: the verbose HTML is pretty ugly when calling 'help' in the terminal...

--- +b0shim/+coils/+greg/Contents.m
+++ +b0shim/+coils/+greg/Contents.m
@@ -1,2 +1,8 @@
-This folder contains classes that define the 8ch neck AC/DC 3T "NP06_ACDC_NECK_8CH" coil.
-For more information about class definition, see [main Readme](https://github.com/neuropoly/realtime_shimming#class-definitions).
+%% greg     8-channel AC/DC 3T neck coil
+% 
+% Nicknamed "greg", after its creator: 
+% [Germain G. et al., ISMRM 2016](https://cds.ismrm.org/protected/16MPresentations/abstracts/1154.html)
+%
+% [Topfer et al., ISMRM 2018](https://www.slideshare.net/neuropoly/integrated-rf-and-shim-coils-for-mri-105560384)
+%
+% <iframe src="//www.slideshare.net/slideshow/embed_code/key/lhvoyKuHLsgc8L" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/neuropoly/integrated-rf-and-shim-coils-for-mri-105560384" title=" Integrated RF and Shim coils for MRI" target="_blank"> Integrated RF and Shim coils for MRI</a> </strong> from <strong><a href="https://www.slideshare.net/neuropoly" target="_blank">NeuroPoly </a></strong> </div>
