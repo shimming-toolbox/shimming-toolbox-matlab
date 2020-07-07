f1a28eb3ed4970d7cf1fcb68f51b0e4d29c4fb87
ryan.topfer@polymtl.ca <ryan.topfer@polymtl.ca>
2020-07-07 15:31:40 -0400

    DOC: Add subpackage +b0shim/+coils/+nibardo with Contents.m for the 7T AC/DC C-spine coil

---
+++ +b0shim/+coils/+nibardo/Contents.m
@@ -0,0 +1,9 @@
+% 15-channel AC/DC 7T c-spine coil
+% 
+% Nicknamed "nibardo", after its creator: See [summary slides](https://www.slideshare.net/neuropoly/20190718-7-tspinecoil)
+%
+% REFERENCE
+%
+% [Lopez-Rios et al., ISMRM 2019](https://cds.ismrm.org/protected/19MPresentations/abstracts/0220.html)
+%
+% <iframe src="//www.slideshare.net/slideshow/embed_code/key/BM50VgTl7437uA" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/neuropoly/20190718-7-tspinecoil" title="20190718 7 t_spine_coil" target="_blank">20190718 7 t_spine_coil</a> </strong> from <strong><a href="https://www.slideshare.net/neuropoly" target="_blank">NeuroPoly </a></strong> </div>
