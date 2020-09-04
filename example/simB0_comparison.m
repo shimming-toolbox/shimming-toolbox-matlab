b5e41a1f8b9466b79c5f5eb89b044add406f97be
Eva <eva.alonso.ortiz@gmail.com>
2020-09-04 14:21:32 -0400

    updated simB0_comparison, since changed were pushed to another branch by mistake

--- example/simB0_comparison.m
+++ example/simB0_comparison.m
@@ -1,39 +1,64 @@
-% Simulate a cylindrical susceptibility distribution at 90 degrees using:
+% generate a cylindrical susceptibility distribution at 90 degrees using:
 % https://github.com/evaalonsoortiz/Fourier-based-field-estimation
-cylindrical_sus_dist = Cylindrical( [128 128 128], [1 1 1], 5, pi/2, [0.36e-6 -8.842e-6]);
+% cylindrical_sus_dist = Cylindrical( [128 128 128], [1 1 1], 5, pi/2, [0.36e-6 -8.842e-6]);
+
+% generate susceptibility distribution for my modified Zubal phantom
+zubal_sus_dist = Zubal('zubal_EAO.nii');
 
 % save as nifti
-cylindrical_sus_dist.save('cylindrical90_R5mm_airMineralOil_ChiDist.nii');
+% cylindrical_sus_dist.save('cylindrical90_R5mm_airMineralOil_ChiDist.nii');
+zubal_sus_dist.save('zubal_EAO_sus.nii');
 
 % compute deltaB0 for the simulated susceptibility distribution using:
 % https://github.com/evaalonsoortiz/Fourier-based-field-estimation
-Bdz = fourier_based_field_est('cylindrical90_R5mm_airMineralOil_ChiDist.nii');
+zubal_dBz = FBFest( zubal_sus_dist.volume, zubal_sus_dist.image_res, zubal_sus_dist.matrix, 'Zubal' );
+zubal_dBz.save('zubal_dBz.nii');
 
 % simulate T2* decay for a cylinder of air surrounded by mineral oil with a
 % deltaB0 found in an external file 
-cylindrical_vol = NumericalModel('Cylindrical3d',128,1,5,90,'Air', 'SiliconeOil');
-cylindrical_vol.generate_deltaB0('load_external', 'Bdz_cylindrical90_R5mm_airMineralOil_ChiDist.nii');
-cylindrical_vol.simulate_measurement(15, [0.001 0.002 0.003 0.004 0.005 0.006], 100);
+% cylindrical_vol = NumericalModel('Cylindrical3d',128,1,5,90,'Air', 'SiliconeOil');
+% cylindrical_vol.generate_deltaB0('load_external', 'Bdz_cylindrical90_R5mm_airMineralOil_ChiDist.nii');
+% cylindrical_vol.simulate_measurement(15, [0.001 0.002 0.003 0.004 0.005 0.006], 100);
 
-% save magnitude and phase data
-magn = cylindrical_vol.getMagnitude;
-phase = cylindrical_vol.getPhase;
+% simulate T2* decay for a modified Zubal phantom with a
+% deltaB0 found in an external file
+zubal_vol = NumericalModel('Zubal','zubal_EAO.nii');
+zubal_vol.generate_deltaB0('load_external', 'zubal_EAO_dBz.nii');
+zubal_vol.simulate_measurement(15, [0.001 0.002 0.003 0.004 0.005 0.006], 100);
+
+% get magnitude and phase data
+magn = zubal_vol.getMagnitude;
+phase = zubal_vol.getPhase;
 compl_vol = magn.*exp(1i*phase);
-% cylindrical_vol.save('Magnitude','SimMag_cylindrical90_R5mm_airMineralOil.nii');
-% cylindrical_vol.save('Phase','SimPh_cylindrical90_R5mm_airMineralOil.nii');
+
 
 % calculate the deltaB0 map from the magnitude and phase data
-[dual_echo_delf] = B0_dual_echo(compl_vol(:,:,:,1:2), [0.001 0.002]);
+[dual_echo_delf] = +imutils.b0.dual_echo(compl_vol(:,:,:,1:2), [0.001 0.002]);
 [multi_echo_delf] = +imutils.b0.multiecho_linfit(compl_vol, [0.001 0.002 0.003 0.004 0.005 0.006]); 
 
-dual_echo_B0_ppm = (dual_echo_delf/3)*(1/42.58e6);
-multi_echo_B0_ppm = (multi_echo_delf/3)*(1/42.58e6);
+dual_echo_b0_ppm = 1e6*(dual_echo_delf/3)*(1/42.58e6);
+multi_echo_b0_ppm = 1e6*(multi_echo_delf/3)*(1/42.58e6);
 
-nii_vol = make_nii(imrotate(fliplr(dual_echo_B0_ppm), -90));
-save_nii(nii_vol, ['dualechoB0_ppm_cylindrical90_R5mm_airMineralOil' '.nii']);
+% calc diff between dual-echo and multi-echo
+diff_dualecho = (dual_echo_b0_ppm-1e6.*real(zubal_dBz.volume));
+figure; imagesc(squeeze(diff_dualecho(:,:,64))); colorbar; title('dual echo - true dBz');
 
-nii_vol = make_nii(imrotate(fliplr(multi_echo_B0_ppm), -90));
-save_nii(nii_vol, ['multiechoB0_ppm_cylindrical90_R5mm_airMineralOil' '.nii']);
+diff_multiecho = (multi_echo_b0_ppm-1e6.*real(zubal_dBz.volume));
+figure; imagesc(squeeze(diff_multiecho(:,:,64))); colorbar; title('multi echo - true dBz');
+
+% save b0 maps
+nii_vol = make_nii(dual_echo_b0_ppm);
+save_nii(nii_vol, ['dualechoB0_ppm_zubal' '.nii']);
+
+nii_vol = make_nii(multi_echo_b0_ppm);
+save_nii(nii_vol, ['multiechoB0_ppm_zubal' '.nii']);
+
+% nii_vol = make_nii(imrotate(fliplr(dual_echo_B0_ppm), -90));
+% save_nii(nii_vol, ['dualechoB0_ppm_cylindrical90_R5mm_airMineralOil' '.nii']);
+% 
+% nii_vol = make_nii(imrotate(fliplr(multi_echo_B0_ppm), -90));
+% save_nii(nii_vol, ['multiechoB0_ppm_cylindrical90_R5mm_airMineralOil' '.nii']);
+
 
 
 
