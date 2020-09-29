9802b44914b79f43f92019da0271aaaacfb6c15b
Eva <eva.alonso.ortiz@gmail.com>
2020-09-28 21:17:33 -0400

    added a handedness variable to switch between left- and right-handed systems

--- +imutils/+b0/dual_echo.m
+++ +imutils/+b0/dual_echo.m
@@ -21,6 +21,8 @@ function [b0] = dual_echo(complVol, delt)
 %     field map in units of Hz 
 % 
 
+handedness = 'left'; % Siemens & Canon = 'left', GE & Philips = 'right' 
+            
 % create magnitude and phase data volumes
 mag_data = abs(complVol);
 ph_data = angle(complVol);
@@ -28,6 +30,13 @@ ph_data = angle(complVol);
 % ph_data differnece reconstruction: Handbook of MRI pulse sequences S.13.5.1
 Z1(:,:,:) = mag_data(:,:,:,1).*exp(1i*ph_data(:,:,:,1));
 Z2(:,:,:) = mag_data(:,:,:,2).*exp(1i*ph_data(:,:,:,2));
-dPhi(:,:,:) = atan2(imag(Z1(:,:,:).*conj(Z2(:,:,:))),real(Z1(:,:,:).*conj(Z2(:,:,:))));
+
+switch handedness
+    case 'left'
+        dPhi(:,:,:) = atan2(imag(Z2(:,:,:).*conj(Z1(:,:,:))),real(Z2(:,:,:).*conj(Z1(:,:,:))));
+    case 'right'
+        dPhi(:,:,:) = atan2(imag(Z1(:,:,:).*conj(Z2(:,:,:))),real(Z1(:,:,:).*conj(Z2(:,:,:))));
+end
+
 b0(:,:,:) = dPhi(:,:,:)./(delt(2)-delt(1)); % [rad*Hz]
 b0 = b0/(2*pi); % [Hz]
