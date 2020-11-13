e6f941e8c7b2772e780f23780237f176b9b3ae21
alexandrebiomed <65645417+alexandrebiomed@users.noreply.github.com>
2020-11-13 16:11:58 -0500

    Première version corrigée 1.2

--- +imutils/+b0/SWFM.m
+++ +imutils/+b0/SWFM.m
@@ -1,4 +1,4 @@
-function [b0] = SWFM(complVol, TE)
+function [b0final] = SWFM(complVol, TE)
 % SWFM computes c0 fieldmaps based on "Selective-Weighted-Field-Mapping"
 % method.
 %
@@ -27,6 +27,8 @@ factor= 1./(gamma.*pi.*TE(end));
 % create magnitude and phase data volumes
 mag_data = abs(complVol);
 ph_data = angle(complVol);
+figure(20); imagesc(mag_data(:,:,1,1));
+figure(21); imagesc(ph_data(:,:,1,1));
 
 X=[];
 for t=1:length(TE)
@@ -41,24 +43,24 @@ end
 % somme des bruits du signal.
 
 % Calcul du facteur FM 
-magtotal=0;
-FM=[];
+magtotal=zeros( size(mag_data,1), size(mag_data,2), size(mag_data,3)  );
+FM=zeros(size(mag_data));
 for t=1:length(TE)
-    magtotal=magtotal+abs(mag_data(:,:,:,t));
+    magtotal(:,:,:)=magtotal(:,:,:)+mag_data(:,:,:,t);
 end
 for t=1:length(TE)
-    FM(:,:,:,t)=abs(mag_data(:,:,:,t))./magtotal;
+    FM(:,:,:,t)=mag_data(:,:,:,t)./magtotal;
 end
 
     % Calcul du facteur FB présent dans la fonction arctangente à 4 cadrants
-sigma=[];
-FB=[];
-sigmatot=0;
+sigma=zeros( size(mag_data)  );
+FB=zeros(size(mag_data));
+sigmatot=zeros( size(mag_data,1), size(mag_data,2), size(mag_data,3)  );
 for t=1:length(TE)
     sigma(:,:,:,t) = stdfilt(mag_data(:,:,:,t));
 end
 for t=1:length(TE)
-    sigmatot=sigmatot+sigma(:,:,:,t);
+    sigmatot(:,:,:)=sigmatot(:,:,:) + sigma(:,:,:,t);
 end
 for t =1:length(TE) 
     FB(:,:,:,t)=sigmatot./sigma(:,:,:,t);
@@ -66,15 +68,15 @@ end
 
 
 % Calcul du facteur SDQWF
-SDQWF=[];
+SDQWF=zeros(size(mag_data));
 for t=1:length(TE)
     SDQWF(:,:,:,t)=FM(:,:,:,t).*FB(:,:,:,t);
 end
     
 % Calcul du facteur produit X1Xj présent dans la fonction arctangente à 4 cadrants
-X1Xt=[];
+X1X_T=[];
 for t=1:length(TE)
-    X1Xt(:,:,:,t)= mag_data(:,:,:,1).*mag_data(:,:,:,t).*exp(1i*(ph_data(:,:,:,1)+ph_data(:,:,:,t)));
+    X1X_T(:,:,:,t)= mag_data(:,:,:,1).*mag_data(:,:,:,t).*exp(1i*(ph_data(:,:,:,1)+ph_data(:,:,:,t)));
 end
 
 % Calcul du facteur somme présent dans la fonction arctangente à 4 cadrants        
@@ -87,22 +89,22 @@ cond4 = zeros( size(complVol) );
 cond5 = zeros( size(complVol) );
 
 for t = 1:length(TE)
-    cond1(:,:,:,t) = real(X1Xt(:,:,:,t))>0 ;
-    cond2(:,:,:,t) = real(X1Xt(:,:,:,t))<0 & imag(X1Xt(:,:,:,t))~=0 ;
-    cond3(:,:,:,t) = real(X1Xt(:,:,:,t))>0 & imag(X1Xt(:,:,:,t))==0 ;
-    cond4(:,:,:,t) = real(X1Xt(:,:,:,t))<0 & imag(X1Xt(:,:,:,t))==0 ;
-    cond5(:,:,:,t) = real(X1Xt(:,:,:,t))==0 & imag(X1Xt(:,:,:,t))==0 ;
+    cond1(:,:,:,t) = real(X1X_T(:,:,:,t))>0 ;
+    cond2(:,:,:,t) = real(X1X_T(:,:,:,t))<0 & imag(X1X_T(:,:,:,t))~=0 ;
+    cond3(:,:,:,t) = real(X1X_T(:,:,:,t))>0 & imag(X1X_T(:,:,:,t))==0 ;
+    cond4(:,:,:,t) = real(X1X_T(:,:,:,t))<0 & imag(X1X_T(:,:,:,t))==0 ;
+    cond5(:,:,:,t) = real(X1X_T(:,:,:,t))==0 & imag(X1X_T(:,:,:,t))==0 ;
     for i= 1:size(complVol,1) 
         for j= 1:size(complVol,2)
             for k= 1: size(complVol,3)
                 if cond1(i,j,k,t)==1
-                     sommable(i,j,k,t) = (imag(X1Xt(i,j,k,t)).*SDQWF(i,j,k,t))./(sqrt( (imag(X1Xt(i,j,k,t))).^2 + (real(X1Xt(i,j,k,t))).^2 ) +real(X1Xt(i,j,k,t)) );
+                     sommable(i,j,k,t) = (imag(X1X_T(i,j,k,t)).*SDQWF(i,j,k,t))./(sqrt( (imag(X1X_T(i,j,k,t))).^2 + (real(X1X_T(i,j,k,t))).^2 ) +real(X1X_T(i,j,k,t)) );
                 elseif cond2(i,j,k,t)==1
-                    sommable(i,j,k,t) = ((sqrt((imag( X1Xt(i,j,k,t) ) ).^2 + (real(X1Xt(i,j,k,t))).^2 ) - real(X1Xt(i,j,k,t))) .*SDQWF(i,j,k,t))./(imag(X1Xt(i,j,k,t)));
+                    sommable(i,j,k,t) = ((sqrt((imag( X1X_T(i,j,k,t) ) ).^2 + (real(X1X_T(i,j,k,t))).^2 ) - real(X1X_T(i,j,k,t))) .*SDQWF(i,j,k,t))./(imag(X1X_T(i,j,k,t)));
                 elseif cond3(i,j,k,t)==1
-                    sommable(i,j,k,t)=pi/2;
+                    sommable(i,j,k,t)=0;
                 elseif cond4(i,j,k,t)==1
-                    sommable(i,j,k,t)=-pi/2;
+                    sommable(i,j,k,t)=0;
                 elseif cond5(i,j,k,t)==1
                     sommable(i,j,k,t)=0;
                 end
@@ -113,26 +115,39 @@ end
                     
 
 for t=1:length(TE)
-    somme(:,:,:) = somme(:,:,:) + squeeze(sommable(:,:,:,t));
+    somme(:,:,:) = somme(:,:,:) + sommable(:,:,:,t);
 end
 
-% Somme n'a pas que des valeurs comprises entre 180 et -180 degrés. Il faut
-% faire en sorte que ce soit le cas ?! 
-for i = 1 : size( somme, 1 )
-    for j = 1 : size( somme, 2 )
-        for k = 1 : size( somme, 3 )
-            if somme(i,j,k)>180 
-                while somme(i,j,k)>180
-                    somme(i,j,k)=somme(i,j,k)-180;
-                end
-            elseif somme(i,j,k)<-180 
-                while somme(i,j,k)<-180
-                    somme(i,j,k)=somme(i,j,k)+180;
-                end
-            end
-        end
-    end
-end
+b01(:,:,:) = factor.*atan(somme); % [rad*Hz]
+b01 = b01/(2*pi); % [Hz]
+figure(22); imagesc(b01);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% % Somme n'a pas que des valeurs comprises entre 180 et -180 degrés. Il faut
+% % faire en sorte que ce soit le cas ?! 
+% for i = 1 : size( somme, 1 )
+%     for j = 1 : size( somme, 2 )
+%         for k = 1 : size( somme, 3 )
+%             if somme(i,j,k)>180 
+%                 while somme(i,j,k)>180
+%                     somme(i,j,k)=somme(i,j,k)-180;
+%                 end
+%             elseif somme(i,j,k)<-180 
+%                 while somme(i,j,k)<-180
+%                     somme(i,j,k)=somme(i,j,k)+180;
+%                 end
+%             end
+%         end
+%     end
+% end
+b02(:,:,:) = factor.*atan(somme); % [rad*Hz]
+b02 = b02/(2*pi); % [Hz]
+figure(23); imagesc(b02);
+
+b03(:,:,:) = b02-b01; % [rad*Hz]
+b03 = b03/(2*pi); % [Hz]
+figure(24); imagesc(b03);
 
 % Je possède des valeurs d'angle donnée par "somme". Or, la fonction atan2
 % à 4 quadrants prend en entrées la partie imaginaire Y et la partie 
@@ -175,5 +190,7 @@ for i=1:size(somme,1)
 end
 
     
-b0(:,:,:) = factor.*atan2(Yco,Xco); % [rad*Hz]
-b0 = b0/(2*pi); % [Hz]
+b0final(:,:,:) = factor.*atan2(Yco,Xco); % [rad*Hz]
+b0final = b0final/(2*pi); % [Hz]
+
+
